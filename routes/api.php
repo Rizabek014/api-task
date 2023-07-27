a46cf313734509706678e9655c49ea1359cf428d
Rizabek014 <rizabekiitu@gmail.com>
2023-07-27 22:04:19 +0600

    my commit

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::get('/news/{id}', '\App\Http\Controllers\Api\NewsController@show'); //get
 Route::get('/news/header/{header}', '\App\Http\Controllers\Api\NewsController@newsHeader'); //get news by Header
 Route::get('/news/author/{id}', '\App\Http\Controllers\Api\NewsController@author'); //get news by Author
 Route::get('/news/rubric/{id}', '\App\Http\Controllers\Api\NewsController@rubric'); //get news by Rubric
-Route::get('/news/rubrics/{id}', '\App\Http\Controllers\Api\NewsController@rubrics'); //get news by Rubric and Child Rubric
+Route::get('/news/rubrics/{id}', '\App\Http\Controllers\Api\NewsController@rubrics'); //get news by Rubric and Child Rubrichttps://github.com/Rizabek014/api-task
 Route::get('/authors', '\App\Http\Controllers\Api\AuthorsController@index'); //get All Authors
 
 Route::post('/add/news', '\App\Http\Controllers\Api\NewsController@store'); //Add news
